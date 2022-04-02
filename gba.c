4c367ef3d7c9109393f0303ba4abd04940297808
Anthony Hong <ahong75@gatech.edu>
2022-04-02 19:06:00 -0400

    Implemented functions in gba.c

--- gba.c
+++ gba.c
@@ -4,15 +4,12 @@ volatile unsigned short *videoBuffer = (volatile unsigned short *) 0x6000000;
 u32 vBlankCounter = 0;
 
 void waitForVBlank(void) {
-  // TODO: IMPLEMENT
-
-  // (1)
   // Write a while loop that loops until we're NOT in vBlank anymore:
   // (This prevents counting one VBlank more than once if your app is too fast)
-  while (*SCANLINECOUNTER > 160);
+  while (SCANLINECOUNTER > 160);
 
   // Write a while loop that keeps going until we're in vBlank:
-  while (*SCANLINECOUNTER < 160);
+  while (SCANLINECOUNTER < 160);
 
   // Finally, increment vBlank:
   vBlankCounter++;
@@ -27,38 +24,40 @@ static int qran(void) {
 int randint(int min, int max) { return (qran() * (max - min) >> 15) + min; }
 
 void setPixel(int row, int col, u16 color) {
-  // TODO: IMPLEMENT
-  *
-  UNUSED(row);
-  UNUSED(col);
-  UNUSED(color);
+  videoBuffer[OFFSET(row, col, WIDTH)] = color; // equivalent to *(videoBuffer + OFFSET() = color), setting the pointer's value
 }
 
 void drawRectDMA(int row, int col, int width, int height, volatile u16 color) {
-  // TODO: IMPLEMENT
-  UNUSED(row);
-  UNUSED(col);
-  UNUSED(width);
-  UNUSED(height);
-  UNUSED(color);
+  // test if we actually need this statement below later
+  DMA[DMA_CHANNEL_3].cnt = 0; // prevent DMA from drawing before we set up src and dst 
+  // We use DMA_CHANNEL_3 because that is the one for general purpose copies of memory
+  DMA[DMA_CHANNEL_3].src = &color; // color holds the address of the color we want, which is why it's volatile bc the compiler might never initalize the variable otherwise
+  for (int i = 0; i < height; i++) { // iterating through each row of the rectangle
+    DMA[DMA_CHANNEL_3].dst = videoBuffer[OFFSET(row + i, col, WIDTH)]; // filling in the current row
+    DMA.cnt = width | DMA_SOURCE_FIXED | DMA_DESTINATION_INCREMENT | DMA_ON; // ORing a bunch of things we want for the control regsiter
+  }
 }
 
 void drawFullScreenImageDMA(const u16 *image) {
-  // TODO: IMPLEMENT
-  UNUSED(image);
+  DMA[DMA_CHANNEL_3].cnt = 0; // prevent DMA from drawing before we set up src and dst 
+  DMA[DMA_CHANNEL_3].src = image;
+  DMA[DMA_CHANNEL_3].dst = videoBuffer;
+  DMA.cnt = WIDTH*HEIGHT | DMA_SOURCE_INCREMENT | DMA_DESTINATION_INCREMENT | DMA_ON;
 }
 
+// Nearly identical to the logic of drawRectDMA
 void drawImageDMA(int row, int col, int width, int height, const u16 *image) {
-  // TODO: IMPLEMENT
-  UNUSED(row);
-  UNUSED(col);
-  UNUSED(width);
-  UNUSED(height);
-  UNUSED(image);
+  DMA[DMA_CHANNEL_3].cnt = 0; // prevent DMA from drawing before we set up src and dst 
+  DMA[DMA_CHANNEL_3].src = image;
+  for (int i = 0; i < height; i++) {
+    DMA[DMA_CHANNEL_3].dst = videoBuffer[OFFSET(row + i, col, WIDTH)];
+    DMA.cnt = width | DMA_SOURCE_FIXED | DMA_DESTINATION_INCREMENT | DMA_ON;
+  }
 }
 
 void undrawImageDMA(int row, int col, int width, int height, const u16 *image) {
-  // TODO: IMPLEMENT
+  // Used to remove the trails left behind of previous images of a moving object/sprite
+  // Not needed because we're going to have a background color, so we can just use the drawRectDMA method instead
   UNUSED(row);
   UNUSED(col);
   UNUSED(width);
@@ -67,8 +66,10 @@ void undrawImageDMA(int row, int col, int width, int height, const u16 *image) {
 }
 
 void fillScreenDMA(volatile u16 color) {
-  // TODO: IMPLEMENT
-  UNUSED(color);
+  DMA[DMA_CHANNEL_3].cnt = 0; // prevent DMA from drawing before we set up src and dst 
+  DMA[DMA_CHANNEL_3].src = &color; 
+  DMA[DMA_CHANNEL_3].dst = videoBuffer;
+  DMA.cnt = WIDTH*HEIGHT | DMA_SOURCE_FIXED | DMA_DESTINATION_INCREMENT | DMA_ON; // WIDTH*HEIGHT indicates the number of pixels/indices we want to fill
 }
 
 void drawChar(int row, int col, char ch, u16 color) {
