c5008efc852b2a1a7dafb14a067f132f52b790d0
Anthony Hong <ahong75@gatech.edu>
2022-04-03 19:23:47 -0400

    Added boss images

---
+++ images/boss.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=30x58 boss boss.png 
+ * Time-stamp: Sunday 04/03/2022, 23:23:33
+ * 
+ * Image Information
+ * -----------------
+ * boss.png 30@58
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef BOSS_H
+#define BOSS_H
+
+extern const unsigned short boss[1740];
+#define BOSS_SIZE 3480
+#define BOSS_LENGTH 1740
+#define BOSS_WIDTH 30
+#define BOSS_HEIGHT 58
+
+#endif
+
